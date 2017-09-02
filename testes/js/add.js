8d16f3de154798077a0185f9dc9926d06a74d2b6
Rafael Dias <ruds.devel@gmail.com>
2017-09-02 12:28:42 -0300

    Add/Remove nomes populares [teste].

--- testes/js/add.js
+++ testes/js/add.js
@@ -4,48 +4,47 @@ Add = {
 
         let btnAdd = document.querySelector('#add-popular-name');
 
-        console.log('Iniciando ADD!');
-
         Add.addNome();
+        Add.generateJSON();
+        document.addEventListener( "click", Add.removeNome );
     },
 
     addNome : function(){
+
         let nameInput = document.querySelector('#pop-name'); 
+        let countryInput = document.querySelector('#pop-name-country');
         let btnAdd = document.querySelector('#add-pop-name');
 
-
         btnAdd.addEventListener('click',function(event) {
-            
             let name = nameInput.value;
-
-            console.log(name);
-
-            Add.createBox(name);
-
+            let country = countryInput.value;
+            Add.createBox(name, country);
         });
-
     },
 
-    removeNome : function(){
-        
+    removeNome : function(event){
+
+        let element = event.target;
+
+        if(element.classList.contains('close-box-name'))    
+            element.parentNode.remove();
     },
 
 
-    createBox : function(name){
+    createBox : function(name, country){
 
         const container  = document.querySelector('#container-pop-name');
 
-        //console.log(container);
-
         let box =  `
             <div class="box-name">
-                <span>${name}</span>
+                <span class="pop-name">${name}</span>
+                <input type="hidden" class="pop-name-country" value="${country}">
+                <span class="close-box-name">X</span>
             </div>    
         `;
 
         Add.appendHtml(container,box);
-
-
+        Add._clearPopField();
     },
 
     appendHtml : function (el, str) {
@@ -54,8 +53,38 @@ Add = {
         while (div.children.length > 0) {
           el.appendChild(div.children[0]);
         }
-    }
+    },
 
+
+    generateJSON : function () {
+
+        let btnGen = document.querySelector('#gen-json-pop');
+
+        btnGen.addEventListener('click', function(){
+           
+            let boxNames = Array.from(document.querySelectorAll('.box-name'));
+
+            let objNames =  boxNames.map((box)=>{
+                return {
+                    popularName: box.querySelector('span.pop-name').textContent,
+                    country: box.querySelector('.pop-name-country').value
+                }
+            });
+
+            let jsonNames = JSON.stringify(objNames);
+
+            console.log(jsonNames);
+
+            return jsonNames;
+        });
+    },
+
+
+    _clearPopField : function () {
+        let inputName = document.querySelector('#pop-name');
+        inputName.value = '';
+        inputName.focus();
+    }
 }
 
 Add.init();
